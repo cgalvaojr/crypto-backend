2984c675018c908989319a2f3c7278c4c990220e
cgalvaojr <cirogalvao@biud.com.br>
2024-03-13 17:33:08 -0300

    feat: esqueleto de controllers, models, requests e testes

---
+++ app/Http/Controllers/CryptocurrencyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCryptocurrencyRequest;
+use App\Http\Requests\UpdateCryptocurrencyRequest;
+use App\Models\Cryptocurrency;
+
+class CryptocurrencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCryptocurrencyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cryptocurrency $cryptocurrency)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCryptocurrencyRequest $request, Cryptocurrency $cryptocurrency)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cryptocurrency $cryptocurrency)
+    {
+        //
+    }
+}
